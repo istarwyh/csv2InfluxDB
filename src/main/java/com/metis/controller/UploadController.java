a02657cc7e645581787c53ce2fac165386322943
istarwyh <yihui.wang@pku.edu.cn>
2020-12-02 00:26:17 +0800

    Add: ~

--- src/main/java/com/metis/controller/UploadController.java
+++ src/main/java/com/metis/controller/UploadController.java
@@ -1,10 +1,6 @@
 package com.metis.controller;
 
-import com.lkx.util.ExcelUtil;
 import com.metis.controller.api.Upload;
-import com.metis.entity.excel.ProvinceAndItem;
-import com.metis.entity.excel.OutOfPovertyRegion;
-import com.metis.entity.excel.RegionOfChina;
 import com.metis.service.InfluxClientBO;
 import com.metis.dto.KeyValueDTO;
 import com.metis.paas.Utils;
@@ -69,46 +65,5 @@ class UploadController implements Upload {
             List<KeyValueDTO> keyValueList = Utils.transfer(csvLists);
             model.addAttribute("lineprotocalData",keyValueList);
             return "showOf";
-    }
-
-    @PostMapping(value = "/test")
-    public  String testImport(@RequestParam(value = "file")  MultipartFile file, Model model) throws Exception{
-        List<OutOfPovertyRegion> outOfPovertyRegionList = this.getOutOfPoverty(file);
-        model.addAttribute("resList",outOfPovertyRegionList);
-        return "test";
-    }
-
-    private List<OutOfPovertyRegion>  getOutOfPoverty(MultipartFile file) throws Exception{
-         /*
-          注意读取时文件表头可能因为隐藏的格式问题读取失败,此时可重写
-         */
-        List<ProvinceAndItem> excelList = ExcelUtil.readXls( file.getBytes(), ProvinceAndItem.class);
-        List<RegionOfChina> regionOfChinaList = ExcelUtil.readXls( folderPath2+"中国省市区.xlsx",RegionOfChina.class);
-        HashMap<String, String> countyCityMap = new HashMap<>(1024);
-        for( RegionOfChina row : regionOfChinaList ){
-            countyCityMap.put( row.getCounty(),row.getCity());
-        }
-        List<OutOfPovertyRegion> resList = new ArrayList<>();
-        String pattern = "(\\d{4})";
-        Pattern p = Pattern.compile(pattern);
-        Matcher m = p.matcher(Objects.requireNonNull(file.getOriginalFilename(),"file没有标识"));
-        String year = "null";
-//        find()这一步与Set的contains()方法不一样,它同时也是寻找匹配的过程.没有这一步将没有分组.
-        if( m.find()) {
-            year= m.group();
-        }
-        for (ProvinceAndItem row : excelList) {
-            String[] counties = row.getItem().split("、");
-            for (String s : counties) {
-                OutOfPovertyRegion res = new OutOfPovertyRegion();
-                res.setProvince(row.getProvince());
-                res.setCity( countyCityMap.getOrDefault(s,"null"));
-                res.setCounty(s);
-                res.setOutOfPovertyYear(year);
-                resList.add(res);
-            }
-        }
-        ExcelUtil.exportExcel(folderPath2+ year +"res.xlsx",resList, OutOfPovertyRegion.class);
-        return resList;
     }
 }
