bc09f79690b5a96df404d2581f7a7a5ab235d206
wangyihui <yihui.wang@pku.edu.cn>
2020-12-02 20:06:34 +0800

    Modify: Something annotation

--- src/main/java/com/metis/annotation/KthLog.java
+++ src/main/java/com/metis/annotation/KthLog.java
@@ -4,7 +4,9 @@ import java.lang.annotation.*;
 
 /**
  * @author wangyihui
- * 注意因为ComponentScan的关系，KthLog必须在
+ * 注意因为ComponentScan的关系，KthLog必须在annotation目录下？
+ * 这个注解只能放在方法上： @Target(ElementType.METHOD)
+ * 这个注解要加载到 JVM 内存：@Retention
  */
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
