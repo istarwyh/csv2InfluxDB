030d05c8afb23bd836b4c99c2d59f1b431fdf254
istarwyh <yihui.wang@pku.edu.cn>
2021-02-26 21:12:26 +0800

    Add: IOC realization

--- src/main/java/com/metis/MetisApplication.java
+++ src/main/java/com/metis/MetisApplication.java
@@ -6,8 +6,12 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
- * 告诉Spring递归地搜索metis包和其子包中直接或间接标记为@Component的类。
- */
+ * 1. 告诉Spring递归地搜索metis包和其子包中直接或间接标记为@Component的类
+ * 2. 将上述被特定注解的类通过反射实例化(Class->Object),以Map形式保存.Map的key为类名,value为类对象.
+ * 3. 再一次遍历上述类获取其中所有字段,
+ *      如果字段key上有@Autowired或@Resource->从Bean容器中拿到对应的对象value,也有可能先拿到的是接口,那么获取接口对应实现类再将其实例化取值
+ * 4. 完成对象都放入IOC容器中
+*/
 @ComponentScan
 /**
  * 基于classpath的内容执行合理的默认行为,例如
